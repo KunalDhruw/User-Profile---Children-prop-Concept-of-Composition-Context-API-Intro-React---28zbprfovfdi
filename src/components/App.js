313bb4638a91355a4351b71eaf34a41f0dab5213
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-25 21:18:10 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+import { UserProfile } from './UserProfile';
+
+
+const App = () => {
+
+  return (
+    <div id="main">
+      
+        <UserProfile />
+      
+    </div>
+  )
+}
+
+
+export default App;
+export {UserContext}
---
+++ src/components/UserProfile.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import React from 'react'
+import { UserContext } from "./App"
+
+const UserProfile = () =>{
+
+}
+
+export {UserProfile}
