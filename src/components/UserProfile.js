d9ae1fc244b313002b9196c77e796130d5cfac2a
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-27 19:14:48 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,15 +1,15 @@
-import { useContext } from "react";
-import React from "react";
-import { UserContext } from "./App";
+import { useContext } from 'react'
+import React from 'react'
+import { UserContext } from "./App"
 
-const UserProfile = () => {
-  const { name, age } = useContext(UserContext);
+const UserProfile = () =>{
+  const {name, age} = useContext(UserContext)
 
   return (
     <div>
-      <h1 id="name">Name: {name} </h1>
-      <h1 id="age">Age: {age} </h1>
+      <h1 id="name">Name:- {name} </h1>
+      <h1 id="age">Age:- {age} </h1>
     </div>
-  );
-};
-export { UserProfile };
+  )
+}
+export {UserProfile}
